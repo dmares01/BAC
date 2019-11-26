205691d9f081c04c05ddbc0e1ebb7e347328b0cf
Drew Mares <dmares01@hamline.edu>
2019-11-26 09:53:06 -0600

    Added another view controller

---
+++ BAC Calculator/BAC Calculator/CalculateController.swift
@@ -0,0 +1,33 @@
+//
+//  CalculateController.swift
+//  BAC Calculator
+//
+//  Created by Live the Fourth on 11/25/19.
+//  Copyright © 2019 Creative Mobile Solutions. All rights reserved.
+//
+
+import UIKit
+
+class CalculateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //put logic here to determine which screen to load when user opens app.
+    var firstTime : Bool = true
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
