e30643e5b41d3c336394b25a435e48bb1fd43dfa
Drew Mares <dmares01@hamline.edu>
2019-12-13 11:36:30 -0600

    updating repo

--- BAC Calculator/BAC Calculator/FavoritesViewController.swift
+++ BAC Calculator/BAC Calculator/FavoritesViewController.swift
@@ -2,19 +2,41 @@
 //  FavoritesViewController.swift
 //  BAC Calculator
 //
-//  Created by MItch on 12/11/19.
+//  Created by MItch on 12/13/19.
 //  Copyright © 2019 Creative Mobile Solutions. All rights reserved.
 //
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+     func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteCell", for: indexPath)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+    
+    
+    
     
 
     /*
